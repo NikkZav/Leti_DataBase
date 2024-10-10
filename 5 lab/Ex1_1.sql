f797006f04a59cdb30d35fa0535478df16811187
Nikita Zavyalov <nikitazavyalov.it@gmail.com>
2024-10-10 16:27:28 +0300

    Update reports: lab1 (_v03), lab2 (_v02)

--- 5 lab/Ex1_1.sql
+++ 5 lab/Ex1_1.sql
@@ -5,8 +5,6 @@ FROM
 	(SELECT juv.adult_member_no,
 			COUNT(*) AS No_Of_Children
 	FROM library.dbo.juvenile AS juv
-	JOIN library.dbo.adult AS adu
-		ON adu.member_no = juv.adult_member_no
 	GROUP BY juv.adult_member_no
 	HAVING COUNT(*) > 3) AS SubQuery
 JOIN library.dbo.adult AS adu
